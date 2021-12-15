f6c75984cb463ee7e10b8a7e6a0b0b6bf000a3f8
etienne-chartrand <etiennechartrand@outlook.fr>
2021-12-15 05:39:20 -0500

    Etienne. Debut Prog

---
+++ BoulogneAntiStressProject/Assets/Scripts/MainBtnClick.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBtnClick : MonoBehaviour
+{
+
+    public GameObject textBox;
+
+    public void ClickBtn()
+    {
+        GlobalCookies.CookieCount += 1;
+    }
+
+}
---
+++ BoulogneAntiStressProject/Assets/Scripts/SellCookie.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SellCookie : MonoBehaviour
+{
+
+    public GameObject textBox;
+    public GameObject statusBox;
+
+    public void ClickBtn()
+    {
+        if(GlobalCookies.CookieCount == 0)
+        {
+            statusBox.GetComponent<Text>().text = "Not enough cookies to sell.";
+            statusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else
+        {
+            GlobalCookies.CookieCount -= 1;
+            GlobalCash.CashCount += 1;
+        }
+        
+
+
+
+    }
+
+}
