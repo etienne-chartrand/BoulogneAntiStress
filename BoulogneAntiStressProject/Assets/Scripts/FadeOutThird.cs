47ca435c0b6892e816e3017cc0c715aa2d78f730
etienne-chartrand <etiennechartrand@outlook.fr>
2021-12-16 15:21:47 -0500

    Fadeoutfinished

--- BoulogneAntiStressProject/Assets/Scripts/FadeOutThird.cs
+++ BoulogneAntiStressProject/Assets/Scripts/FadeOutThird.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FadeOut : MonoBehaviour
+public class FadeOutThird : MonoBehaviour
 {
     private float fadeSpeed = 1;
+    public static bool firstIsOut = false;
+    public static bool secondIsOut = false;
+    public static bool thirdIsOut = false;
+
     public GameObject cube;
 
     private void Update()
-    {
-        if ((Timer.timer / 2) * 1.5 <= Timer.middleTimer)
+    {   
+        if (Timer.timer <= Timer.minTimer)
         {
             Color objectColor = cube.GetComponent<Renderer>().material.color;
             float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
