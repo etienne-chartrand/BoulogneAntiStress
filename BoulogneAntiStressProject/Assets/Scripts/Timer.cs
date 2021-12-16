c6bded3cad7f3621c81a6873c920253ec11e13f6
etienne-chartrand <etiennechartrand@outlook.fr>
2021-12-16 00:07:03 -0500

    Update Timer.cs

--- BoulogneAntiStressProject/Assets/Scripts/Timer.cs
+++ BoulogneAntiStressProject/Assets/Scripts/Timer.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     private bool isPaused = false;
-
+    private Material material;
+    private float fadeSpeed = 2;
 
     private float timer = 1200;
     private float maxtimer;
@@ -48,15 +49,40 @@ public class Timer : MonoBehaviour
 
         if ((timer/2)* 1.5 <= middleTimer )
         {
-            firstCube.SetActive(false);
+            Color objectColor = firstCube.GetComponentInChildren<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+
+            int numOfChildrens = transform.childCount;
+            for(int i = 0; 1 <numOfChildrens; i++)
+            {
+                GameObject child = transform.GetChild(i).gameObject;
+                //child.GetComponent<Renderer>().materials
+            }
+
+
+            
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            firstCube.GetComponentInChildren<Renderer>().material.color = objectColor;
+
+
         }
         if(timer <= middleTimer)
         {
-            secondCube.SetActive(false);
+            Color objectColor = secondCube.GetComponentInChildren<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            secondCube.GetComponentInChildren<Renderer>().material.color = objectColor;
         }
         if(timer <= minTimer)
         {
-            thirdCube.SetActive(false);
+            Color objectColor = thirdCube.GetComponentInChildren<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            thirdCube.GetComponentInChildren<Renderer>().material.color = objectColor;
         }
     }
 
