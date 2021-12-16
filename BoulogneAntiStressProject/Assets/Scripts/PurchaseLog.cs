a346342bf6cfc6a067a1b31749b96c154411d7f8
etienne-chartrand <etiennechartrand@outlook.fr>
2021-12-16 16:02:34 -0500

    UpdateScene

--- BoulogneAntiStressProject/Assets/Scripts/PurchaseLog.cs
+++ BoulogneAntiStressProject/Assets/Scripts/PurchaseLog.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PurchaseLog : MonoBehaviour
 {
     public GameObject Timerstart;
+    public GameObject inGameScene;
+    public GameObject mainMenuScene;
     
     //active le timer GameObject
     public void StartAutoCookie()
     {
         Timerstart.SetActive(true);
+        inGameScene.SetActive(true);
+        mainMenuScene.SetActive(false);
     }
 }
